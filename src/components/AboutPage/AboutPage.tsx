757436ce008503b89653d02c39af09a481ff9813
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-06-26 12:03:45 +0200

    Update about page

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -5,42 +5,46 @@ export const AboutPage = () => {
     return (
         <Box className="page-content__container" maxW="800px" mx="auto" py={10} px={6} bg="white" borderRadius="lg" >
             <VStack align="start" spacing={6}>
-                <Text fontSize="lg">
-                    Die <b>Developer Network Bundles</b> von con terra sind eine Sammlung modularer Erweiterungen für die Plattform <b><i>map.apps</i></b>,
-                    die Entwickler:innen und Anwender:innen helfen, GIS-Anwendungen effizienter zu gestalten und individuell anzupassen.
-                    Diese Bundles sind über das GitHub-Profil von con terra verfügbar:
-                    <Link href="https://github.com/conterra" color="#005587" isExternal ml={1}>https://github.com/conterra</Link>.
-                </Text>
-                <Divider />
-                <Heading size="md" color="#005587">Was sind Developer Network Bundles?</Heading>
+                <Heading size="md" color="#005587">Was sind die Developer Network Bundles?</Heading>
                 <Text>
-                    Developer Network Bundles sind vorgefertigte Funktionsbausteine, die in <b><i>map.apps</i></b> integriert werden können.
+                    Die <b>Developer Network Bundles</b> sind eine Sammlung modularer Erweiterungen für die Plattform <b>map.apps</b>&nbsp;
+                    der con terra, die Entwickler:innen und Anwender:innen helfen, GIS-Anwendungen effizienter zu gestalten und individuell anzupassen.<br />
                     Sie dienen dazu, den Funktionsumfang von Anwendungen zu erweitern oder bestimmte Aufgaben zu vereinfachen.
-                    Die Bundles sind modular aufgebaut, was eine flexible Kombination und Wiederverwendung ermöglicht.
+                    Die Bundles sind modular aufgebaut, was eine flexible Kombination und Wiederverwendung ermöglicht.<br />
+                    Entwickelt werden die Bundles gemeinsam mit der Community. Eine Übersicht über die Bundles ist in der<Link href="https://conterra.github.io/#/overview" color="#005587" isExternal ml={1} textDecoration="underline">Bundle-Übersicht</Link> verfügbar. Die Dazugehörigen Entwicklungsprojekte finden Sie auf der <Link href="https://github.com/conterra" color="#005587" isExternal ml={1} textDecoration="underline">GitHub Seite der con terra</Link>.
                 </Text>
                 <Divider />
                 <Heading size="md" color="#005587">Vorteile der Nutzung</Heading>
-                <List spacing={2} pl={2}>
-                    <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Modularität:</b> Flexible Kombination und Wiederverwendung.</ListItem>
-                    <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Einfache Integration:</b> Bereitstellung als ZIP-/JAR-Dateien.</ListItem>
-                    <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Zentrale Verwaltung:</b> Einfaches Update und Wartung.</ListItem>
-                    <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Open Source:</b> Anpassungen durch die Community möglich.</ListItem>
-                </List>
+                <Box w="100%">
+                    <List
+                        spacing={2}
+                        pl={2}
+                        display="grid"
+                        gridTemplateColumns={{ base: '1fr', md: '1fr 1fr' }}
+                        gap={2}
+                        sx={{
+                            '& > li': { marginTop: '0 !important' },
+                            '& > li:nth-of-type(2)': { marginTop: '0 !important' },
+                            '& > li:nth-of-type(3)': { marginTop: '0 !important' },
+                            '& > li:nth-of-type(4)': { marginTop: '0 !important' }
+                        }}
+                    >
+                        <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Modularität:</b><br />Flexible Kombination und Wiederverwendung.</ListItem>
+                        <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Einfache Integration:</b><br />Bereitstellung als ZIP-/JAR-Dateien.</ListItem>
+                        <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Zentrale Verwaltung:</b><br />Einfaches Update und Wartung.</ListItem>
+                        <ListItem><ListIcon as={CheckCircleIcon} color="green.400" /> <b>Open Source:</b><br />Anpassungen durch die Community möglich.</ListItem>
+                    </List>
+                </Box>
                 <Divider />
-                <Heading size="md" color="#005587">Einstieg und weitere Informationen</Heading>
+                <Heading size="md" color="#005587">Einstieg und Beteiligung</Heading>
                 <Text>
-                    Für Entwickler:innen bietet das Projekt <b><i>mapapps-4-developers</i></b> einen Einstiegspunkt zum Erstellen eigener Bundles und Themes.
-                    Es enthält Beispiele für gängige Aufgaben wie das Erstellen von Widgets mit Vue.js oder das Anpassen eigener Themes.
-                </Text>
-                <Text>
-                    Weitere Informationen und eine Übersicht der verfügbaren Bundles finden sich auf dem GitHub-Profil von con terra:
-                    <Link href="https://github.com/conterra" color="#005587" isExternal ml={1}>https://github.com/conterra</Link>.
+                    Für Entwickler:innen bietet das Projekt <Link href="https://github.com/conterra/mapapps-4-developers" color="#005587" isExternal ml={1} textDecoration="underline">mapapps-4-developers</Link> einen Einstiegspunkt zum Erstellen eigener Bundles und Themes.
+                    Dort finden Sie einige Beispiele für gängige Aufgaben.<br />
+                    Gerne können Sie sich auch über Issues und mit Ideen beteiligen. Nutzen Sie dazu das GitHub Issue Feature oder treten Sie per<Link href="mailto:developernetwork@conterra.de" color="#005587" isExternal ml={1} textDecoration="underline">Mail</Link> mit uns in Kontakt.
                 </Text>
                 <Box bg="gray.50" borderRadius="md" p={4} w="100%">
                     <InfoIcon color="#005587" mr={2} />
-                    Die Developer Network Bundles bieten eine effektive Möglichkeit, <b><i>map.apps</i></b>-Anwendungen individuell zu gestalten
-                    und den Funktionsumfang gezielt zu erweitern. Durch die modulare Struktur und die offene Bereitstellung fördern sie
-                    eine flexible und nachhaltige Entwicklung von GIS-Anwendungen für Entwickler:innen und Anwender:innen gleichermaßen.
+                    Da die Developer Network Bundles durch die Community entwickelt werden, besteht bei Fehlern kein Anspruch auf Standard-Support. Wir unterstützen Sie jedoch gerne bei der Nutzung und Integration der Bundles in Ihre Anwendungen. <br />
                 </Box>
             </VStack>
         </Box>
